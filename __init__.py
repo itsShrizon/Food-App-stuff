567607500af638804093081b924fc05c53a6348f
Kawsar <diu.kawsar@gmail.com>
2026-01-18 15:30:22 +0600

    __init__ done

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .onboarding.flow import onboarding
 from .onboarding.start import start_onboarding
 from .chatbot import ai_chatbot
+from .meal_generator import generate_meal_plan
 
-__all__ = ['onboarding', 'start_onboarding', 'ai_chatbot'] 
+__all__ = ['onboarding', 'start_onboarding', 'ai_chatbot', 'generate_meal_plan'] 
